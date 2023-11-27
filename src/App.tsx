d3466db49de0a1edf9fb46f4412909b3cc5e555e
John de Stigter <john.s@getjobber.com>
2023-11-27 09:47:42 -0700

    Fake events

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,64 @@
-import { addDays, format, startOfMonth, startOfWeek } from "date-fns";
+import {
+  isSameDay,
+  addDays,
+  format,
+  startOfMonth,
+  startOfWeek,
+} from "date-fns";
 import { useState } from "react";
 import "./styles.scss";
 import classNames from "./Grid.module.scss";
 import c from "classnames";
-import { motion, LayoutGroup } from "framer-motion";
+import { motion } from "framer-motion";
+
+function getInitialDate() {
+  const date = new Date();
+  date.setDate(24);
+  return startOfWeek(date);
+}
+
+const randomTitles = [
+  "Lawn mowing",
+  "Gardening",
+  "Car washing",
+  "House cleaning",
+  "Dog walking",
+  "Cat walking",
+  "Dog walking",
+  "Building",
+];
+
+function getRandomTitle() {
+  return randomTitles[Math.floor(Math.random() * randomTitles.length)];
+}
+
+function randomEvent() {
+  const date = new Date(2023, 10, 1);
+  date.setDate(Math.floor(Math.random() * 30) + 1);
+
+  const start = new Date(date);
+  start.setHours(Math.floor(Math.random() * 12));
+
+  const end = new Date(start);
+  end.setHours(start.getHours() + 1);
+
+  return {
+    id: Math.random(),
+    start,
+    end,
+    title: getRandomTitle(),
+    color: randomColor(),
+  };
+}
+
+const events = Array.from({ length: 50 }).map(randomEvent);
+
+events.forEach(event => {
+  console.log(event.start, event.end, event.title)
+})
 export default function App() {
-  const [selectedDate, onSelectDate] = useState(() => {
-    const date = new Date();
-    date.setDate(24);
-    return startOfWeek(date);
-  });
-  
+  const [selectedDate, onSelectDate] = useState(getInitialDate);
+
   const [view, setView] = useState<"month" | "week" | "day">("month");
 
   const startDate =
@@ -118,8 +166,39 @@ function Grid({ view, startDate, onSelectDate }: PropsGrid) {
           <div>{format(date, "d")}</div>
         </motion.div>
 
+        {events
+          .filter(({ start }) => isSameDay(start, date))
+          .map((event) => {
+            const duration = event.end.getTime() - event.start.getTime();
+            const hours = duration / 1000 / 60 / 60;
+            const height = hours * 100;
+            const top = ((event.start.getTime() - date.getTime()) / 1000 / 60 / 60) * 100
+            return (
+              <motion.div
+                layoutId={""+event.id}
+                layout
+                animate={{ opacity: [0, 1] }}
+                key={event.id}
+                className={classNames.event}
+                style={{
+                  backgroundColor: event.color,
+                  color: getTextColor(event.color),
+                  position: isMonthView ? "relative" : "absolute",
+                  top: isMonthView ? 0 : top,
+                  height: isMonthView ? "auto" : height,
+                }}
+              >
+                <motion.div layout>
+                  <div>
+                    <div>{event.title}</div>
+                  </div>
+                </motion.div>
+              </motion.div>
+            );
+          })}
+
         {/* {((view !== "day" && date.getDate() === 24) || (view === "day" && team[index].name === "John")) && ( */}
-        {date.getDate() === 24 && (
+        {/* {date.getDate() === 24 && (
           <motion.div
             layoutId="event"
             layout
@@ -139,7 +218,7 @@ function Grid({ view, startDate, onSelectDate }: PropsGrid) {
               </div>
             </motion.div>
           </motion.div>
-        )}
+        )} */}
       </div>
     );
   });
@@ -154,9 +233,7 @@ function Grid({ view, startDate, onSelectDate }: PropsGrid) {
     >
       {!isMonthView && <Hours />}
 
-      <div className={classNames.days}>
-        {cells}
-      </div>
+      <div className={classNames.days}>{cells}</div>
     </div>
   );
 }
@@ -175,4 +252,14 @@ function Hours() {
 
 function randomColor() {
   return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+function getTextColor(color: string) {
+  const hex = color.replace("#", "");
+  const r = parseInt(hex.substr(0, 2), 16); // Grab the hex representation of red (chars 1-2) and convert to decimal (base 10).
+  const g = parseInt(hex.substr(2, 2), 16);
+  const b = parseInt(hex.substr(4, 2), 16);
+
+  const yiq = (r * 299 + g * 587 + b * 114) / 1000; // http://en.wikipedia.org/wiki/YIQ
+  return yiq >= 128 ? "black" : "white";
 }
